3fff2e34768f8789007329d9151dd031bf792662
Kostenitskiy Alexander <kostenitskiy.alexander.work@gmail.com>
2025-05-07 18:06:44 +0300

    Add project files

---
+++ src/Redis/Interfaces/IRedisService.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace Redis.Interfaces;
+
+public interface IRedisService
+{
+    Task<T?> GetStringAsync<T>(string key);
+    
+    Task<Dictionary<string, T>> GetStringsDictionaryAsync<T>(string pattern);
+
+    Task<bool> SetStringAsync<T>(
+        string key, 
+        T data, 
+        JsonSerializerOptions? options = null, 
+        TimeSpan? expireTime = null);
+}
